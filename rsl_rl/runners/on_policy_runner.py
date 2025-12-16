3c3f8077ac179f49a7be354df771c519249b0cdb
hudduuy <18273520106@163.com>
2025-12-16 17:21:19 +0800

    修复加载模型时的权重参数，移除不必要的weights_only参数

--- rsl_rl/runners/on_policy_runner.py
+++ rsl_rl/runners/on_policy_runner.py
@@ -219,7 +219,7 @@ class OnPolicyRunner:
             }, path)
 
     def load(self, path, load_optimizer=True):
-        loaded_dict = torch.load(path,weights_only=True)
+        loaded_dict = torch.load(path)
         self.alg.actor_critic.load_state_dict(loaded_dict['model_state_dict'])
         if load_optimizer:
             self.alg.optimizer.load_state_dict(loaded_dict['optimizer_state_dict'])
